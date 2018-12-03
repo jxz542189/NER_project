01a1da10b99e296ebc106a7546bad3170b14f65d
jxz542189 <1318394945@qq.com>
2018-12-03 16:38:54 +0800

    删除bert_blstm_crf

--- bert_blstm_crf/utils/ConfigError.py
+++
@@ -1,8 +0,0 @@
-class ConfigError(Exception):
-    """Any configuration error."""
-    def __init__(self, message):
-        super(ConfigError, self).__init__()
-        self.message = message
-
-    def __str__(self):
-        return repr(self.message)
